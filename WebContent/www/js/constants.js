d1a3720136d1a1d4d259dedaf6abf7cd7969f085
daniegarcia254 <daniegarcia254@gmail.com>
2016-06-16 01:28:48 +0200

    Set config url to production as default

--- WebContent/www/js/constants.js
+++ WebContent/www/js/constants.js
@@ -1,8 +1,8 @@
 UZCampusWebMapApp.constant('APP_CONSTANTS', {
-    //URI_busquedas: 'http://155.210.14.31:8080/mapa/busquedas',
-    //URI_estancias: 'http://155.210.14.31:8080/mapa/estancias',
-    URI_busquedas: 'http://localhost:8080/busquedas',
-    URI_estancias: 'http://localhost:8080/estancias',
+    URI_busquedas: 'http://155.210.14.31:8080/mapa/busquedas',
+    URI_estancias: 'http://155.210.14.31:8080/mapa/estancias',
+    //URI_busquedas: 'http://localhost:8080/busquedas',
+    //URI_estancias: 'http://localhost:8080/estancias',
     URI_fotos: 'http://155.210.14.31:8080/mapa/www/fotos/',
     URI_Geoserver: 'http://155.210.14.31:8080/geoserver/',
     edificios: ["CSF_1018_00","CSF_1110_00","CSF_1106_00","CSF_1021_00","CSF_1095_00","CSF_1097_00","CSF_1017_00","CSF_1022_00"],
